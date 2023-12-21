ac8389a6bdce34b2bfba7c95b221e2ecd58bb391
Vasco Ceti <vasco.ceti@gmail.com>
2023-12-21 18:08:09 +0100

    day4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header/Header";
 import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Please select a tab");
+
+  const handleClick = (e) => {
+    const selectedButton = e.target.textContent;
+    setSelectedTab(selectedButton);
+  };
+
   return (
     <div>
       <Header />
@@ -18,11 +26,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>Jsx</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleClick}>Components</TabButton>
+            <TabButton onSelect={handleClick}>Jsx</TabButton>
+            <TabButton onSelect={handleClick}>Props</TabButton>
+            <TabButton onSelect={handleClick}>State</TabButton>
           </menu>
+          {selectedTab}
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-const TabButton = ({ children }) => {
+const TabButton = ({ children, onSelect }) => {
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={onSelect}>{children}</button>
     </li>
   );
 };
